a3121a59b9b470e2156f65bf170f22a06aea395f
Sanyam Gupta <sanyamgupta19@gmail.com>
2020-12-11 00:55:36 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req,res){
+    // will return something to the browser
+    return res.end('<h1>Express is up for Codeial!</h1>');
+}
+
+// module.exports.actionName = function(req,res){}
