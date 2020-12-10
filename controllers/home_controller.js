6c08b811bd43c0653dcb449630a2412817ff63e0
Sanyam Gupta <sanyamgupta19@gmail.com>
2020-12-11 03:19:54 +0530

    Create a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,11 @@
 module.exports.home = function(req,res){
     // will return something to the browser
-    return res.end('<h1>Express is up for Codeial!</h1>');
+    // return res.end('<h1>Express is up for Codeial!</h1>');
+    
+    // instead rendering an ejs file
+    return res.render('home',{
+        title : "Home"
+    }); 
 }
 
 // module.exports.actionName = function(req,res){}
